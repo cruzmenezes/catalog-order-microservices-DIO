2421d65f884cc4655a68ee49e215a60e8c8d8264
cruzmenezes <cruz.menezes@outlook.com>
2025-07-17 18:13:31 -0300

    upload springBoot

---
+++ desafio-spring-dio-ntt/src/main/java/com/desafioDioNtt/desafio_spring_dio_ntt/DesafioSpringDioNttApplication.java
@@ -0,0 +1,13 @@
+package com.desafioDioNtt.desafio_spring_dio_ntt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringDioNttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringDioNttApplication.class, args);
+	}
+
+}
---
+++ desafio-spring-dio-ntt/src/test/java/com/desafioDioNtt/desafio_spring_dio_ntt/DesafioSpringDioNttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioDioNtt.desafio_spring_dio_ntt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringDioNttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
