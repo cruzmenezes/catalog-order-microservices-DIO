ede0e61a686acab87b8f4da08787f46721db6f7c
cruzmenezes <cruz.menezes@outlook.com>
2025-07-17 18:42:18 -0300

    estruturando pasta

--- desafio-spring-dio-ntt/src/main/java/com/desafioDioNtt/desafio_spring_dio_ntt/DesafioSpringDioNttApplication.java
+++ desafio-spring-dio-ntt/src/main/java/com/desafioDioNtt/desafio_spring_dio_ntt/DesafioSpringDioNttApplication.java
@@ -2,7 +2,9 @@ package com.desafioDioNtt.desafio_spring_dio_ntt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DesafioSpringDioNttApplication {
 
